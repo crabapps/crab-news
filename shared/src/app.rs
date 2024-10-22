6a1d6e288bc059d8b7a518ea2b467e2fa383dea3
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2024-10-22 18:21:34 +0200

    saving unfinished progress
    adding feeds or folders is still adding an
    outline... try to use <T> and DRY

--- shared/src/app.rs
+++ shared/src/app.rs
@@ -8,11 +8,20 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Event {
-    ImportSubscriptions, // shows up in Menu -> File
-    ExportSubscriptions, // shows up in Menu -> File
-    AddNewFeed,          // account | root | folder // shows up in Menu -> File
+    ImportSubscriptions,
+    ExportSubscriptions,
+    // Feeds
+    // TO ADD AN OUTLINE TO ROOT USE https://docs.rs/opml/1.1.6/opml/struct.OPML.html#method.add_feed
+    // TO ADD AN OUTLINE TO FOLDER USE https://docs.rs/opml/1.1.6/opml/struct.Outline.html#method.add_feed
+    AddNewFeed, // account | root | folder // shows up in Menu -> File
     DeleteFeed,
     RenameFeed,
+    MoveFeedToFolder, // location -> root | folder
+    // FeedStore -> root + 1st level folder. no more
+    // THIS ADDS AN OUTLINE TO OPML::Body
+    AddNewFolder, // shows up in Menu -> File
+    DeleteFolder,
+    RenameFolder,
 
     // EVENTS LOCAL TO THE CORE
     #[serde(skip)]
@@ -50,7 +59,11 @@ impl App for CrabNews {
             Event::AddNewFeed => todo!(),
             Event::DeleteFeed => todo!(),
             Event::RenameFeed => todo!(),
+            Event::MoveFeedToFolder => todo!(),
             Event::Fetch(_) => todo!(),
+            Event::AddNewFolder => todo!(),
+            Event::DeleteFolder => todo!(),
+            Event::RenameFolder => todo!(),
         };
 
         caps.render.render();
@@ -67,39 +80,34 @@ mod test {
     use super::*;
     // use crux_core::{assert_effect, testing::AppTester};
 
-    // #[test]
-    // fn renders() {
-    //     let app = AppTester::<CrabNews, _>::default();
-    //     let mut model = Model::default();
-
-    //     let update = app.update(Event::Reset, &mut model);
-
-    //     // Check update asked us to `Render`
-    //     assert_effect!(update, Effect::Render(_));
-    // }
-
-    // SUBSCRIPTIONS with opml crate
     #[test]
-    // FIXME rather import opml directly into Vec<OPML> with all the right FeedStore locations folders
     fn import_subscriptions() {
+        let model: Model = Model::default();
+        let mut subscriptions: Vec<OPML> = model.subscriptions;
         let mut file = std::fs::File::open("example.opml").unwrap();
         let document = OPML::from_reader(&mut file).unwrap();
-        let example_xml = r#"<?xml version="1.0" encoding="ISO-8859-1"?><opml version="2.0"><head><title>Subscriptions.opml</title><dateCreated>Sat, 18 Jun 2005 12:11:52 GMT</dateCreated><ownerName>Crab News</ownerName></head><body><outline text="Feed Name" title="Feed Name" description="" type="rss" version="RSS" htmlUrl="https://example.com/" xmlUrl="https://example.com/atom.xml"/><outline text="Group Name" title="Group Name"><outline text="Feed Name" title="Feed Name" description="" type="rss" version="RSS" htmlUrl="https://example.com/" xmlUrl="https://example.com/rss.xml"/></outline></body></opml>"#;
-        let xml = OPML::from_str(example_xml).unwrap();
-        assert_eq!(xml, document);
+        let example_feed = r#"<?xml version="1.0" encoding="ISO-8859-1"?><opml version="2.0"><head><title>Subscriptions.opml</title><dateCreated>Sat, 18 Jun 2005 12:11:52 GMT</dateCreated><ownerName>Crab News</ownerName></head><body><outline text="Feed Name" title="Feed Name" description="" type="rss" version="RSS" htmlUrl="https://example.com/" xmlUrl="https://example.com/atom.xml"/><outline text="Group Name" title="Group Name"><outline text="Feed Name" title="Feed Name" description="" type="rss" version="RSS" htmlUrl="https://example.com/" xmlUrl="https://example.com/rss.xml"/></outline></body></opml>"#;
+
+        subscriptions.push(document);
+
+        let added_feed = subscriptions.first().unwrap().clone();
+        let expected_feed = OPML::from_str(example_feed).unwrap();
+
+        assert_eq!(added_feed, expected_feed);
     }
 
     #[test]
     fn export_subscriptions() {}
 
+    // TODO use Events::AddNewFeed(FeedStore::Root)
     #[test]
-    fn add_new_subscription() {
+    fn add_new_root_feed() {
         let model: Model = Model::default();
         let mut subscriptions: Vec<OPML> = model.subscriptions;
         let mut new_sub: OPML = OPML::default();
 
         new_sub.add_feed("Feed Name", "https://example.com/");
-        subscriptions.insert(0, new_sub);
+        subscriptions.push(new_sub);
 
         let added_feed = subscriptions
             .first()
@@ -116,5 +124,50 @@ mod test {
         };
 
         assert_eq!(added_feed, expected_feed);
+    }
+
+    // TODO use Events::AddNewFeed(FeedStore::Folder)
+    #[test]
+    fn add_new_nested_feed() {
+        // let model: Model = Model::default();
+        // let subscriptions: Vec<OPML> = model.subscriptions;
+        let mut group = Outline::default();
+
+        // <outline text="Group Name" title="Group Name">
+        group.add_feed("Feed Name", "https://example.com/");
+
+        let added_feed = group.outlines.first().unwrap();
+        let expected_feed = &Outline {
+            text: "Feed Name".to_string(),
+            xml_url: Some("https://example.com/".to_string()),
+            ..Outline::default()
+        };
+
+        assert_eq!(added_feed, expected_feed);
+    }
+
+    // TODO use Events::AddNewFolder
+    #[test]
+    fn add_new_folder() {
+        let new_sub: OPML = OPML::default();
+        let mut model: Model = Model::default();
+        model.subscriptions.push(new_sub);
+        let mut body: opml::Body = model.subscriptions.first().unwrap().body.clone();
+        let new_folder = &Outline {
+            text: "Folder Name".to_string(),
+            title: Some("Folder Name".to_string()),
+            ..Outline::default()
+        };
+
+        body.outlines.push(new_folder.clone());
+
+        let added_folder = body.outlines.first().unwrap();
+        let expected_folder = &Outline {
+            text: "Folder Name".to_string(),
+            title: Some("Folder Name".to_string()),
+            ..Outline::default()
+        };
+
+        assert_eq!(added_folder, expected_folder);
     }
 }
